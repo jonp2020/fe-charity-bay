236ebf044e9d18ec16eceb5a8852ffa3042628a2
alihusnain4190 <alihusnain4190@gmail.com>
2020-11-06 16:52:47 +0000

    add css to make responsive and regionDrop down where user can select location of own city

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import "./App.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import IndividualItem from './Pages/IndividualItem'
-import LandingPage from './Pages/LandingPage'
+import IndividualItem from "./Pages/IndividualItem";
+import LandingPage from "./Pages/LandingPage";
 import { Router } from "@reach/router";
+import PostIems from "./Pages/PostIems";
 function App() {
   return (
     <div className="App">
@@ -11,6 +12,7 @@ function App() {
       <Router>
         <LandingPage path="/" />
         <IndividualItem path="/item/:item_id" />
+        <PostIems path="/post_item" />
       </Router>
       <Footer />
     </div>
