a97f977d7037cf08cb24bdb27cbb985504008c4d
JFParrott <jfparrott@protonmail.com>
2020-11-06 14:38:14 +0000

    add firebase config and build AuthContext

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import "./App.css";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import IndividualItem from "./Pages/IndividualItem";
-import LandingPage from "./Pages/LandingPage";
-import About from "./Pages/About";
-import { Router } from "@reach/router";
-import Purchase from "./Pages/Purchase";
+import './App.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import IndividualItem from './Pages/IndividualItem';
+import LandingPage from './Pages/LandingPage';
+import About from './Pages/About';
+import { Router } from '@reach/router';
+import Purchase from './Pages/Purchase';
+import { AuthProvider } from './contexts/AuthContext';
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <div className="central-area">
-        <Router primary={false}>
-          <LandingPage path="/" />
-          <IndividualItem path="/item/:item_id" />
-          <About path="/about" />
-          <Purchase path="/purchase/:item_id" />
-        </Router>
+    <AuthProvider>
+      <div className="App">
+        <Header />
+        <div className="central-area">
+          <Router primary={false}>
+            <LandingPage path="/" />
+            <IndividualItem path="/item/:item_id" />
+            <About path="/about" />
+            <Purchase path="/purchase/:item_id" />
+          </Router>
+        </div>
+        <Footer />
       </div>
-      <Footer />
-    </div>
+    </AuthProvider>
   );
 }
 
