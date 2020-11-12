72a5bf97143a56fa4a828766313def5fbc47989f
JFParrott <jfparrott@protonmail.com>
2020-11-12 18:11:04 +0000

    add images to charity page, remove charity name and add minimal styling to carousel sliders

--- src/components/HowItWorks.jsx
+++ src/components/HowItWorks.jsx
@@ -1,12 +1,12 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 import {
   CarouselProvider,
   Slider,
   Slide,
   ButtonBack,
   ButtonNext,
-} from "pure-react-carousel";
-import "pure-react-carousel/dist/react-carousel.es.css";
+} from 'pure-react-carousel';
+import 'pure-react-carousel/dist/react-carousel.es.css';
 
 export default class HowItWorks extends Component {
   render() {
@@ -19,50 +19,50 @@ export default class HowItWorks extends Component {
         totalSlides={9}
         touchEnabled={true}
       >
-        <div className="carousel-container">
+        <div>
           <Slider className="carousel-slider">
-            <Slide index={0}>
+            <Slide className="first-and-fifth" index={0}>
               <p>
                 <span className="lobster-font">CharityBay</span> is a website
                 built on trust.
               </p>
             </Slide>
-            <Slide index={1}>
+            <Slide className="second-and-sixth" index={1}>
               <p>
                 <strong>Sign up</strong> to CharityBay to buy or sell anything
                 you no longer want or need.
               </p>
             </Slide>
-            <Slide index={2}>
+            <Slide className="third-and-seventh" index={2}>
               <p>You can buy and sell items from a variety of categories.</p>
             </Slide>
-            <Slide index={3}>
+            <Slide className="fourth-and-eighth" index={3}>
               <p>
                 You can choose a charity that you would like the money to go to
                 for each item you are selling.
               </p>
             </Slide>
-            <Slide index={4}>
+            <Slide className="first-and-fifth" index={4}>
               <p>
                 If you want to buy an item, you will be directed to the Just
                 Giving donate page that was selected by the seller. Here you can
                 donate the money for that item.
               </p>
             </Slide>
-            <Slide index={5}>
+            <Slide className="second-and-sixth" index={5}>
               <p>
                 Once you have made your donation, you will be taken back to
                 CharityBay where you will need to confirm your purchase in order
                 to alert the seller.
               </p>
             </Slide>
-            <Slide index={6}>
+            <Slide className="third-and-seventh" index={6}>
               <p>
                 Both the buyer and seller will receive email notifications with
                 each other's contact details.
               </p>
             </Slide>
-            <Slide index={7}>
+            <Slide className="fourth-and-eighth" index={7}>
               <p>
                 The seller will arrange for the item to be delivered to the
                 buyer.
