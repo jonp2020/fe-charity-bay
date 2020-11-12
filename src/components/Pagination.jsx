854f8c058f8d08522f85c8a9da53b90f47f184f3
alihusnain4190 <alihusnain4190@gmail.com>
2020-11-12 12:48:17 +0000

    add css style in pagination and set drop down menu in landing page

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import { red } from "@material-ui/core/colors";
+import React from "react";
 
 export default function Pagination({
   page,
@@ -8,33 +9,41 @@ export default function Pagination({
   changePage,
 }) {
   return (
-    <div>
+    <div className="paggination">
       <button
+        className="p-button"
         disabled={atStart}
         onClick={() => {
           changePage(page - 1);
         }}
       >
-        {'<'}
+        {"<"}
       </button>
       {pages.map((singlePage) => (
         <button
+          style={{
+            background: "rgb(250,237,205)",
+            padding:'6px 12px',
+            margin: "2px"
+          }}
+          className="p-button"
           key={singlePage}
           onClick={() => {
             changePage(singlePage);
           }}
-          className={page === singlePage ? 'button-background' : null}
+          className={page === singlePage ? "button-background" : null}
         >
           {singlePage}
         </button>
       ))}
       <button
+        className="p-button"
         disabled={atEnd}
         onClick={() => {
           changePage(page + 1);
         }}
       >
-        {'>'}
+        {">"}
       </button>
     </div>
   );
